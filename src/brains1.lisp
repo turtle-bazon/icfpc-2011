4a571ead06e3f17476a21bc773e058229ddccb44
Mansur Ziatdinov <gltronred@gmail.com>
2011-06-18 20:56:26 +0400

    Comments added

--- src/brains1.lisp
+++ src/brains1.lisp
@@ -3,10 +3,10 @@
 (defun make-attack-queue-0-0-1 (my-fn)
   "Attacks 0-th slot using our 0-th with attack value 1 (attack 0 0 1)"
   (let ((my (delay (funcall my-fn))))
-    `((:right #'attack-card ,my)
-      (:right #'zero-card   ,my)
-      (:right #'zero-card   ,my)
-      (:left  #'K-card      ,my)
-      (:left  #'S-card      ,my)
-      (:right #'succ-card   ,my)
-      (:right #'zero-card   ,my))))
+    `((:right #'attack-card ,my)     ; 2 0 attack
+      (:right #'zero-card   ,my)     ; 2 0 zero
+      (:right #'zero-card   ,my)     ; 2 0 zero
+      (:left  #'K-card      ,my)     ; 1 K 0
+      (:left  #'S-card      ,my)     ; 1 S 0
+      (:right #'succ-card   ,my)     ; 2 0 succ
+      (:right #'zero-card   ,my))))  ; 2 0 zero
