4e5acca59d4ea8442cb84cf431dad72285584866
Mansur Ziatdinov <gltronred@gmail.com>
2011-06-18 20:46:39 +0400

    Bug fixed in brains1

--- src/brains1.lisp
+++ src/brains1.lisp
@@ -3,7 +3,7 @@
 (defun make-attack-queue-0-0-1 (my-fn)
   "Attacks 0-th slot using our 0-th with attack value 1 (attack 0 0 1)"
   (let ((my (delay (funcall my-fn))))
-    `((:left  #'attack-card ,my)
+    `((:right #'attack-card ,my)
       (:right #'zero-card   ,my)
       (:right #'zero-card   ,my)
       (:left  #'K-card      ,my)
