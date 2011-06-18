7899e2b3cdbbcdd7413da4f54133ad867640cd41
Mansur Ziatdinov <gltronred@gmail.com>
2011-06-19 02:23:27 +0400

    B2 and Y combinators

--- src/brains1.lisp
+++ src/brains1.lisp
@@ -35,6 +35,16 @@
     (:right ,b          ,storage)
     (:right ,c          ,storage)))
 
+(defun b2-combinator (storage b c d)
+  "B2 combinator: B2 a b c = S(K(S(Ka)b))cd = a(b(cd))"
+  `((:left  #'k-card    ,storage)
+    (:left  #'s-card    ,storage)
+    (:right ,b          ,storage)
+    (:left  #'k-card    ,storage)
+    (:right #'s-card    ,storage)
+    (:right ,c          ,storage)
+    (:right ,d          ,storage)))
+
 (defun attack-queue (storage i j n)
   "Function is written to storage; it attacks j-th opponent's slot using i-th our with given value n"
   (unless (/= storage 0) (normal-error))
@@ -46,3 +56,25 @@
 	  (b-combinator storage #'get-card #'zero-card)
 	  (write-number 0 n)
 	  (b-combinator storage #'get-card #'zero-card)))
+
+(defun y-combinator (storage s1 s2 s3)
+  "Y f = f (Y f)
+   Y = S S K (S (K (S S (S (S S K)))) K)"
+  (unless (/= storage 0) (normal-error))
+  (append `((:left  #'put-card   0)
+	    (:left  #'put-card   1)
+	    (:right #'s-card     0)
+	    (:right #'s-card     0)
+	    (:right #'k-card     0)
+	    (:left  #'s-card     0)	; 0 -> s(ssk)
+	    (:right #'s-card     1)
+	    (:left  #'s-card     1))	; 1 -> ss
+	  (b-combinator 1 #'get-card #'zero-card) ; 1 -> ss(s(ssk))
+	  `((:left  #'k-card     1)
+	    (:left  #'s-card     1)
+	    (:right #'k-card     1)
+	    (:left  #'k-card     1)	; 1 -> k(s(k(ss(s(ssk)))) k)
+	    (:left  #'put-card   ,storage)	; storage -> I
+	    (:right #'s-card     ,storage)
+	    (:left  #'s-card     ,storage))
+	  (b2-combinator storage #'get-card #'succ-card #'zero-card)))
