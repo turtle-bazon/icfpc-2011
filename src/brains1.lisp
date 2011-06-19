2bc070ecc818e71234d2a7aca50f02f92af8a0e2
Mansur Ziatdinov <gltronred@gmail.com>
2011-06-19 13:59:42 +0400

    Double attack; doesn't work in our code, but works in their

--- src/brains1.lisp
+++ src/brains1.lisp
@@ -52,8 +52,10 @@
 ;; has to be tested
 (defun y-combinator (storage slot-f)
   "Y f = f (Y f)
-   Y = S S K (S (K (S S (S (S S K)))) K)"
+   Y = S S K (S (K (S S (S (S S K)))) K)
+   Y = WS(BWB)"
   (unless (/= storage 0) (normal-error))
+  (unless (/= storage 1) (normal-error))
   (append `((:left  ,#'put-card   0)
 	    (:left  ,#'put-card   1)
 	    (:right ,#'s-card     0)
@@ -67,31 +69,56 @@
 	    (:left  ,#'s-card     1)
 	    (:right ,#'k-card     1)
 	    (:left  ,#'k-card     1)	; 1 -> k(s(k(ss(s(ssk)))) k)
-	    (:left  ,#'put-card   ,storage)	; storage -> I
+	    (:left  ,#'put-card   ,storage) ; storage -> I
 	    (:right ,#'s-card     ,storage)
-	    (:left  ,#'s-card     ,storage))
-	  (b2-combinator storage #'get-card #'succ-card #'zero-card)
+	    (:left  ,#'s-card     ,storage)) ; storage -> ss
+	  (b2-combinator storage #'get-card #'succ-card #'zero-card) ; storage -> ss(k(s(k(ss(s(ssk))))k))
 	  (write-number 0 slot-f)
 	  (b-combinator 2 #'get-card #'zero-card)))
 
 ;; has to be tested
 (defun w-combinator (storage slot-x slot-y)
   "W x y = x y y
-   W = S S (K (S K K))"
+   W = S S (K (S K K))
+   W x y = S x I y"
   (unless (/= storage 0) (normal-error))
-  (append `((:left  ,#'put-card   0)
-	    (:left  ,#'put-card   1)
-	    (:right ,#'s-card     0)
-	    (:right ,#'k-card     0)
-	    (:right ,#'k-card     0)
-	    (:left  ,#'k-card     0)	; 0 -> k(skk)
-	    (:right ,#'s-card     1)
-	    (:left  ,#'s-card     1))	; 1 -> ss
-	  (b-combinator 1 #'get-card #'zero-card) ; 1 -> ss(k(skk))
+  (append `((:left  ,#'put-card   1)
+	    (:right ,#'s-card     1))
 	  (write-number 0 slot-x)
-	  (b-combinator 1 #'get-card #'zero-card) ; 1 -> W x
+	  (b-combinator 1 #'get-card #'zero-card) ; 1 -> S x
+	  `((:right ,#'i-card      1))		  ; 1 -> S x I
 	  (write-number 0 slot-y)
-	  (b-combinator 1 #'get-card #'zero-card)))
+	  (b-combinator 1 #'get-card #'zero-card))) ; 1 -> S x I y
+
+(defun double-action (storage slot-x slot-y)
+  "x is a function that needs argument y
+   double-action do (x y) twice:
+   F x y = x y (x y) = S x x y"
+  (unless (/= storage 0) (normal-error))
+  (unless (/= storage 1) (normal-error))
+  (append `((:left  ,#'put-card  1)
+	    (:right ,#'s-card    1))
+	  (write-number 0 slot-x)
+	  (b2-combinator 1 #'get-card #'get-card #'zero-card)
+	  (b2-combinator 1 #'get-card #'get-card #'zero-card)
+	  (write-number 0 slot-y)
+	  (b2-combinator 1 #'get-card #'get-card #'zero-card)))
+
+(defun attack-without-value (storage i j)
+  "attack i j"
+  (unless (/= storage 0) (normal-error))
+  (append `((:left  ,#'put-card    ,storage)
+	    (:right ,#'attack-card ,storage))
+	  (write-number 0 i)
+	  (b-combinator storage #'get-card #'zero-card)
+	  (write-number 0 j)
+	  (b-combinator storage #'get-card #'zero-card)))
+
+(defun double-attack (s0 s1 s2 i j n)
+  "s0, s1, s2 -- temporary storages, other parameters as in attack"
+  (append (attack-without-value s1 i j) 
+	  (write-number s2 n nil) 
+	  (double-action s0 s1 s2)))
 
 ;;; doesn't work, has to be rewritten - ?
 ;(defun infinite-attack (storage i j n)
